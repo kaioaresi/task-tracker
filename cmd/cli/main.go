4a8f358064baf2b7ce5ca290869d59bbf33b1d2c
kaio.santos <kaioaresi@gmail.com>
2025-07-28 10:11:29 -0300

    refact: restart

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,29 +1,13 @@
-package main
+package cli
 
 import (
 	"fmt"
-	"log"
-	"task-tracker/internal/cli"
-	"task-tracker/internal/task"
+	"os"
 )
 
 func main() {
-	description := cli.Create()
 
-	if description == "" {
-		log.Fatal("Nenhum valor foi informado, utilize o -h para ver as opções disponíveis!")
-	}
+	task := os.Args[0]
 
-	t1 := task.NewTask(description)
-
-	fmt.Println(t1)
-
-	t1.UpdateStatus()
-
-	fmt.Println(t1)
-
-	t1.Done()
-
-	fmt.Println(t1)
-
+	fmt.Println(task)
 }
--- internal/storage/storage.go
+++
@@ -1,7 +0,0 @@
-package storage
-
-import "task-tracker/internal/task"
-
-type Storage interface {
-	Save(t task.Task) error
-}
